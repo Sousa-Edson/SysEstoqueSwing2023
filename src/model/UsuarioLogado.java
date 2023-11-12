68a8f06ced93b3e00da2a7a350f913d4221473fb
Edson Sousa <edson3711.es@gmail.com>
2023-11-12 13:27:49 -0300

    Feat: criando uma classe de usuario logado

---
+++ src/model/UsuarioLogado.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author edson
+ */
+public class UsuarioLogado {
+
+    private static Usuario usuarioLogado;
+
+    private UsuarioLogado() {
+        // Impede a instanciação da classe (construtor privado)
+    }
+
+    public static void setUsuarioLogado(Usuario usuario) {
+        usuarioLogado = usuario;
+    }
+
+    public static Usuario getUsuarioLogado() {
+        return usuarioLogado;
+    }
+}
